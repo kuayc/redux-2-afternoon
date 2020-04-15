d69287dd74f090a1a072e1018afb0891227d5327
Chin Kuay <kuaychin@gmail.com>
2020-04-15 14:22:53 -0500

    added files

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,52 +1,56 @@
-import React, { Component } from 'react';
-import Background from './../shared/Background/Background'
-import Chart1 from './../shared/Chart1';
-import Chart2 from './../shared/Chart2';
-import AddPurchase from './../shared/AddPurchase';
-import DisplayPurchases from './../shared/DisplayPurchases';
-import Loading from './../shared/Loading/Loading';
-import Nav from './../shared/Nav';
-import './Budget.css';
-import { connect } from 'react-redux'
-
+import React, { Component } from "react";
+import Background from "./../shared/Background/Background";
+import Chart1 from "./../shared/Chart1";
+import Chart2 from "./../shared/Chart2";
+import AddPurchase from "./../shared/AddPurchase";
+import DisplayPurchases from "./../shared/DisplayPurchases";
+import Loading from "./../shared/Loading/Loading";
+import Nav from "./../shared/Nav";
+import "./Budget.css";
+import { connect } from "react-redux";
+// IMPORT THE ACTION CREATOR FROM THE REDUCER FILE FOR USE BY THE CONNECT METHOD, WHICH THEN ADDS THE FUNCTION TO THE PROPS // OBJECT OF THIS COMPONENT
+import { requestUserData } from "./../../ducks/userReducer";
 
 class Budget extends Component {
+  componentDidMount() {
+    // WHEN THE COMPONENT MOUNTS, THE ACTION CREATOR IS INVOKED, THE REDUCER FUNCTION FIRES, AND STATE IS UPDATED ACCORDINGLY   // IN THE REDUX STORE
+    this.props.requestUserData();
+  }
 
   render() {
-  // DESTRUCTURE THE LOADING PROPERTY FROM THE BUDGET OBJECT THAT WAS MAPPED TO PROPS THROUGH MAPSTATETOPROPS/CONNECT
     const { loading } = this.props.budget;
+    const { firstName, lastName } = this.props.user;
     return (
       <Background>
-        // UPDATE THE TERNARY TO CHECK WHETHER OR NOT THE LOADING PROPERTY 
-        // ON THE BUDGET OBJECT IS TRUE. IF SO, THEN THE LOADING COMPONENT
-        // SHOULD BE DISPLAYED
         {loading ? <Loading /> : null}
-        <div className='budget-container'>
-          <Nav />
-          <div className='content-container'>
+        <div className="budget-container">
+          <Nav firstName={firstName} lastName={lastName} />
+          <div className="content-container">
             <div className="purchases-container">
               <AddPurchase />
               <DisplayPurchases />
             </div>
-            <div className='chart-container'>
+            <div className="chart-container">
               <Chart1 />
               <Chart2 />
             </div>
           </div>
         </div>
       </Background>
-    )
+    );
   }
 }
 
-// THIS FUNCTION TAKES IN THE REDUX STORE STATE AND MAPS THE BUDGET REDUCER INFO 
-// FROM THE REDUX STORE TO A BUDGET KEY ON THIS COMPONENT'S PROPS OBJECT
 function mapStateToProps(state) {
   return {
-    budget: state.budget
-  }
+    budget: state.budget,
+    user: state.user,
+  };
 }
 
-	
-// THE CONNECT METHOD TAKES IN THE MAPSTATETOPROPS FUNCTION AND CONNECTS THIS COMPONENT TO THE REDUX STORE
-export default connect(mapStateToProps)(Budget);
+// IN ORDER TO ACCESS THE REQUESTUSERDATA ACTION CREATOR, YOU NEED TO CONNECT IT TO
+// THE REDUCER FUNCTION THROUGH THE CONNECT METHOD. THE CONNECT METHOD ACCEPTS TWO ARGUMENTS, A MAPSTATETOPROPS OBJECT, AND A MAPDISPATCHTOPROPS OBJECT. OUR DISPATCHED ACTIONS GO INSIDE OF THE SECOND ARGUMENT OBJECT AS A KEY/VALUE PAIR.
+export default connect(
+  mapStateToProps,
+  { requestUserData }
+)(Budget);
--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,14 +1,13 @@
-// IMPORT THE REDUX FUNCTIONS, MIDDLEWARE, AND REDUCERS THAT WILL BE USED TO CREATE A STORE
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import promiseMiddleware from 'redux-promise-middleware';
-import budgetReducer from './budgetReducer';
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import promiseMiddleware from "redux-promise-middleware";
+import budgetReducer from "./budgetReducer";
+// IMPORT THE USER REDUCER
+import userReducer from "./userReducer";
 
-
-// USE COMBINE REDUCERS TO COMBINE REDUCERS INTO A SINGLE, "ROOT" REDUCER THAT WILL BE USED BY THE STORE. EVENTUALLY YOU MAY HAVE MULTIPLE REDUCERS IN AN APPLICATION, AND THIS STEP HELPS US COMBINE THOSE INTO ONE ROOT REDUCER THAT CAN BE USED TO CREATE A STORE
+// ADD THE USER REDUCER TO THE ROOT REDUCER OBJECT THAT WILL BE USED TO CREATE THE REDUX STORE STATE
 const rootReducer = combineReducers({
-  budget: budgetReducer
-})
+  budget: budgetReducer,
+  user: userReducer,
+});
 
-// CREATE THE STORE USING THE CREATE STORE FUNCTION. PASS IN THE APPROPRIATE ARGUMENTS.
-// YOU ARE EXPORTING IT SO THAT IT CAN BE USED BY THE PROVIDER COMPONENT LATER ON
 export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
