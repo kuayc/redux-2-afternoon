3aa608aaf4388c894a8b171bcf8838e0cd474d83
Chin Kuay <kuaychin@gmail.com>
2020-04-15 14:15:16 -0500

    fixed budget

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,34 +1,52 @@
-import React, { Component } from "react";
-import Background from "./../shared/Background/Background";
-import Chart1 from "./../shared/Chart1";
-import Chart2 from "./../shared/Chart2";
-import AddPurchase from "./../shared/AddPurchase";
-import DisplayPurchases from "./../shared/DisplayPurchases";
-import Loading from "./../shared/Loading/Loading";
-import Nav from "./../shared/Nav";
-import "./Budget.css";
+import React, { Component } from 'react';
+import Background from './../shared/Background/Background'
+import Chart1 from './../shared/Chart1';
+import Chart2 from './../shared/Chart2';
+import AddPurchase from './../shared/AddPurchase';
+import DisplayPurchases from './../shared/DisplayPurchases';
+import Loading from './../shared/Loading/Loading';
+import Nav from './../shared/Nav';
+import './Budget.css';
+import { connect } from 'react-redux'
+
 
 class Budget extends Component {
+
   render() {
+  // DESTRUCTURE THE LOADING PROPERTY FROM THE BUDGET OBJECT THAT WAS MAPPED TO PROPS THROUGH MAPSTATETOPROPS/CONNECT
+    const { loading } = this.props.budget;
     return (
       <Background>
-        {true ? <Loading /> : null}
-        <div className="budget-container">
+        // UPDATE THE TERNARY TO CHECK WHETHER OR NOT THE LOADING PROPERTY 
+        // ON THE BUDGET OBJECT IS TRUE. IF SO, THEN THE LOADING COMPONENT
+        // SHOULD BE DISPLAYED
+        {loading ? <Loading /> : null}
+        <div className='budget-container'>
           <Nav />
-          <div className="content-container">
+          <div className='content-container'>
             <div className="purchases-container">
               <AddPurchase />
               <DisplayPurchases />
             </div>
-            <div className="chart-container">
+            <div className='chart-container'>
               <Chart1 />
               <Chart2 />
             </div>
           </div>
         </div>
       </Background>
-    );
+    )
   }
 }
 
-export default Budget;
+// THIS FUNCTION TAKES IN THE REDUX STORE STATE AND MAPS THE BUDGET REDUCER INFO 
+// FROM THE REDUX STORE TO A BUDGET KEY ON THIS COMPONENT'S PROPS OBJECT
+function mapStateToProps(state) {
+  return {
+    budget: state.budget
+  }
+}
+
+	
+// THE CONNECT METHOD TAKES IN THE MAPSTATETOPROPS FUNCTION AND CONNECTS THIS COMPONENT TO THE REDUX STORE
+export default connect(mapStateToProps)(Budget);
