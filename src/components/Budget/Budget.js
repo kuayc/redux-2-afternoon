53c69ff280a88f8c8d0d2e10c7ee4ca6ad417b65
Chin Kuay <kuaychin@gmail.com>
2020-04-15 14:25:52 -0500

    changed budget files

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -8,17 +8,18 @@ import Loading from "./../shared/Loading/Loading";
 import Nav from "./../shared/Nav";
 import "./Budget.css";
 import { connect } from "react-redux";
-// IMPORT THE ACTION CREATOR FROM THE REDUCER FILE FOR USE BY THE CONNECT METHOD, WHICH THEN ADDS THE FUNCTION TO THE PROPS // OBJECT OF THIS COMPONENT
 import { requestUserData } from "./../../ducks/userReducer";
+import { requestBudgetData } from "./../../ducks/budgetReducer";
 
 class Budget extends Component {
   componentDidMount() {
-    // WHEN THE COMPONENT MOUNTS, THE ACTION CREATOR IS INVOKED, THE REDUCER FUNCTION FIRES, AND STATE IS UPDATED ACCORDINGLY   // IN THE REDUX STORE
     this.props.requestUserData();
+    this.props.requestBudgetData();
   }
 
   render() {
-    const { loading } = this.props.budget;
+    // DESTRUCTURING VALUES HERE IS OPTIONAL BUT RECOMMENDED FOR CLEANER JSX
+    const { loading, purchases, budgetLimit } = this.props.budget;
     const { firstName, lastName } = this.props.user;
     return (
       <Background>
@@ -28,11 +29,11 @@ class Budget extends Component {
           <div className="content-container">
             <div className="purchases-container">
               <AddPurchase />
-              <DisplayPurchases />
+              <DisplayPurchases purchases={purchases} />
             </div>
             <div className="chart-container">
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit} />
+              <Chart2 purchases={purchases} />
             </div>
           </div>
         </div>
@@ -48,9 +49,7 @@ function mapStateToProps(state) {
   };
 }
 
-// IN ORDER TO ACCESS THE REQUESTUSERDATA ACTION CREATOR, YOU NEED TO CONNECT IT TO
-// THE REDUCER FUNCTION THROUGH THE CONNECT METHOD. THE CONNECT METHOD ACCEPTS TWO ARGUMENTS, A MAPSTATETOPROPS OBJECT, AND A MAPDISPATCHTOPROPS OBJECT. OUR DISPATCHED ACTIONS GO INSIDE OF THE SECOND ARGUMENT OBJECT AS A KEY/VALUE PAIR.
 export default connect(
   mapStateToProps,
-  { requestUserData }
+  { requestUserData, requestBudgetData }
 )(Budget);
